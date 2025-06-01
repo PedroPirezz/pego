db63fad3a83958f674271d79156f0c1359db8d18
PedroPirezz <Foreverp333@gmail.com>
2025-06-01 15:21:41 -0300

    DB resolve

--- ServerModules/Database/Connection/DatabaseConection.js
+++ ServerModules/Database/Connection/DatabaseConection.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
